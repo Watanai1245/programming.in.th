2c56980e7f28f2bd4a1c8b0b6f3cbfa3fdc4b1e8
Watanai1245 <new0820571245@gmail.com>
2021-09-20 13:03:59 +0700

    edit

---
+++ C_Language/0000_A+B Problem.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("result");
+    return 0;
+}
--- Python_Language/0000_A+B Problem.py
+++ Python_Language/0000_A+B Problem.py
@@ -1,4 +1,4 @@
-1 = int(input())
-2 = int(input())
-print(1+2)
+A = int(input()) 
+B = int(input())
+print(A+B)
 
